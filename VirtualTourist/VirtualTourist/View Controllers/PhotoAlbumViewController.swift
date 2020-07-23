07416d12e1150aeb0ace90d64f0226a22741745a
Chantal-Deguire <5s5onscreen@gmail.com>
2020-07-23 14:11:17 -0400

    feat: Initial organization of files + xcdatamodel
    new file:   VirtualTourist/AppDelegate.swift
    new file:   VirtualTourist/VirtualTourist.xcodeproj/project.pbxproj
    new file:   VirtualTourist/VirtualTourist/Assets/Assets.xcassets/
      AppIcon.appiconset/Contents.json
    new file:   VirtualTourist/VirtualTourist/Assets/Assets.xcassets/
      Contents.json
    new file:   VirtualTourist/VirtualTourist/Info.plist
    new file:   VirtualTourist/VirtualTourist/Model/
      VirtualTourist.xcdatamodeld/.xccurrentversion
    new file:   VirtualTourist/VirtualTourist/Model/
      VirtualTourist.xcdatamodeld/VirtualTourist.xcdatamodel/contents
    new file:   VirtualTourist/VirtualTourist/SceneDelegate.swift
    new file:   VirtualTourist/VirtualTourist/Storyboards/Base.lproj/
      LaunchScreen.storyboard
    new file:   VirtualTourist/VirtualTourist/Storyboards/Base.lproj/
      Main.storyboard
    new file:   VirtualTourist/VirtualTourist/View Controllers/
      PhotoAlbumViewController.swift
    new file:   VirtualTourist/VirtualTourist/View Controllers/
      TravelLocationsMapViewController.swift

---
+++ VirtualTourist/VirtualTourist/View Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Chantal Deguire on 2020-07-23.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ VirtualTourist/VirtualTourist/View Controllers/TravelLocationsMapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TravelLocationsMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Chantal Deguire on 2020-07-23.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
