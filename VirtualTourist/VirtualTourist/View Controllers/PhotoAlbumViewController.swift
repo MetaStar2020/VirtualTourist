f4731421198096399757b7c2f796bcd1c75c55bd
Chantal-Deguire <5s5onscreen@gmail.com>
2020-07-29 12:48:15 -0400

    refactor: making small changes with CollectionCell
    Trying to figure out what is the issue with the storyboard as it
    won't recognize the collectionView.
    
    modified:   VirtualTourist/VirtualTourist.xcodeproj/project.pbxproj
    modified:   VirtualTourist/VirtualTourist/Storyboards/Base.lproj/
      Main.storyboard
    modified:   VirtualTourist/VirtualTourist/View Controllers/
      PhotoAlbumViewController.swift
    renamed:    VirtualTourist/VirtualTourist/Views/
      CollectionCell.swift -> VirtualTourist/VirtualTourist/Views/
      CollectionViewCell.swift

--- VirtualTourist/VirtualTourist/View Controllers/PhotoAlbumViewController.swift
+++ VirtualTourist/VirtualTourist/View Controllers/PhotoAlbumViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 import MapKit
 import CoreData
 
-class PhotoAlbumViewController: UIViewController {
+class PhotoAlbumViewController: UICollectionViewController {
     
     //outlet for map here
     //outlet for colection view
     @IBOutlet weak var mapView: MKMapView!
-    //@IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    
+    let collectionCellID = "CollectionViewCell"
     
     var dataController: DataController!
         
@@ -44,6 +46,7 @@ class PhotoAlbumViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
                 
+        print("view did load")
         //Set MapView's delegate and properties
         self.mapView.delegate = self
         self.mapView.isZoomEnabled = false
@@ -82,18 +85,18 @@ class PhotoAlbumViewController: UIViewController {
             
         //MARK: - Using CoreData to populate map with the pin
                     
-            let lat = CLLocationDegrees((pin.value(forKeyPath: "latitude") as? Double) ?? 0.0 )
-            let long = CLLocationDegrees((pin.value(forKeyPath: "longitude") as? Double) ?? 0.0 )
+        let lat = CLLocationDegrees((pin.value(forKeyPath: "latitude") as? Double) ?? 0.0 )
+        let long = CLLocationDegrees((pin.value(forKeyPath: "longitude") as? Double) ?? 0.0 )
                     
-            // The lat and long are used to create a CLLocationCoordinates2D instance.
-            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        // The lat and long are used to create a CLLocationCoordinates2D instance.
+        let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
                     
-            // Creating the annotation and setting its coordiate, title, and subtitle properties
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = coordinate
+        // Creating the annotation and setting its coordiate, title, and subtitle properties
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
                     
-            // Adding the annotation in an array of annotations.
-            annotations.append(annotation)
+        // Adding the annotation in an array of annotations.
+        annotations.append(annotation)
                     
                 
         // When the array is complete, we add the annotations to the map.
@@ -111,6 +114,23 @@ class PhotoAlbumViewController: UIViewController {
     
         }
     }*/
+    
+    //MARK: - Required functions for collection view
+    // Return the number of rows for the table.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1    }
+
+    // Provide a cell object for each row.
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       // Fetch a cell of the appropriate type.
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionCellID , for: indexPath) as! CollectionViewCell
+        print(indexPath)
+       
+       // Configure the cell’s contents.
+       //cell.collectionView.albumPhoto = self.memes[indexPath.row].originalImage
+        
+       return cell
+    }
 }
 
 
